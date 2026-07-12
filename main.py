fe85d7ecc6e93c978f0607a082528045c693d495
alupait884-creator <alupait884@gmail.com>
2026-07-12 16:43:37 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+import os
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler, ContextTypes
+
+TOKEN = os.getenv("TOKEN")
+...poora code...
+app.run_polling()
